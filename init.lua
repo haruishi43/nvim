423f3b57e7ff2a9dd93e86785f0bfefbc519c5ae
Toraudonn <toraudonn@gmail.com>
2022-11-21 16:45:31 +0900

    add configs

---
+++ init.lua
@@ -0,0 +1,21 @@
+-- plugin setup
+require("haruishi.plugins-setup")
+
+-- core
+require("haruishi.core.options")
+require("haruishi.core.keymaps")
+require("haruishi.core.colorscheme")
+
+-- plugins
+require("haruishi.plugins.comment")
+require("haruishi.plugins.nvim-tree")
+require("haruishi.plugins.lualine")
+require("haruishi.plugins.telescope")
+require("haruishi.plugins.nvim-cmp")
+require("haruishi.plugins.lsp.mason")
+require("haruishi.plugins.lsp.lspsaga")
+require("haruishi.plugins.lsp.lspconfig")
+require("haruishi.plugins.lsp.null-ls")
+require("haruishi.plugins.autopairs")
+require("haruishi.plugins.treesitter")
+require("haruishi.plugins.gitsigns")
