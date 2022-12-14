389882ad5079189810cb9db96c2d2b342cef6c0f
Toraudonn <toraudonn@gmail.com>
2022-12-14 10:15:15 +0900

    update copilot script

--- lua/haruishi/plugins/copilot.lua
+++ lua/haruishi/plugins/copilot.lua
@@ -2,10 +2,6 @@ local setup, copilot = pcall(require, "copilot")
 if not setup then
 	return
 end
-local cmp_setup, copilot_cmp = pcall(require, "copilot_cmp")
-if not cmp_setup then
-	return
-end
 
 copilot.setup({
 	suggestion = {
@@ -15,6 +11,11 @@ copilot.setup({
 		markdown = false,
 	},
 })
+
+local cmp_setup, copilot_cmp = pcall(require, "copilot_cmp")
+if not cmp_setup then
+	return
+end
 
 copilot_cmp.setup({
 	method = "getCompletionCycling",
