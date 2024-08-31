109c68ca0753f80a934cd86a4e80c3eec2d4e7b5
Toraudonn <toraudonn@gmail.com>
2024-08-31 12:58:34 +0900

    update init

--- lua/haruishi/plugins-setup.lua
+++ lua/haruishi/plugins-setup.lua
@@ -56,7 +56,7 @@ return packer.startup(function(use)
 	-- fuzzy finding w/ telescope
 	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
 	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
-	use({ "nvim-telescope/telescope-bibtex.nvim", cond = fn.has("mac") == 1 })
+	use("nvim-telescope/telescope-bibtex.nvim")
 
 	-- autocompletion
 	use("hrsh7th/nvim-cmp") -- completion plugin
