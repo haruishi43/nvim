97e8f7a709e3b2b1142abfe382631bafaecab18b
Toraudonn <toraudonn@gmail.com>
2024-10-06 23:37:01 +0900

    update ripgrep search

--- lua/haruishi/plugins/telescope.lua
+++ lua/haruishi/plugins/telescope.lua
@@ -27,10 +27,43 @@ telescope.setup({
 			"%.png$",
 			".DS_Store",
 		},
-	},
+    vimgrep_arguments = {
+      "rg",
+      "--follow",        -- Follow symbolic links
+      "--hidden",        -- Search for hidden files
+      "--no-heading",    -- Don't group matches by each file
+      "--with-filename", -- Print the file path with the matched lines
+      "--line-number",   -- Show line numbers
+      "--column",        -- Show column numbers
+      "--smart-case",    -- Smart case search
+
+      -- Exclude some patterns from search
+      "--glob=!**/.git/*",
+      "--glob=!**/.idea/*",
+      "--glob=!**/.vscode/*",
+      "--glob=!**/build/*",
+      "--glob=!**/dist/*",
+      "--glob=!**/yarn.lock",
+      "--glob=!**/package-lock.json",
+    },
+  },
   pickers = {
     find_files = {
-      find_command = {'rg', '--files', '--hidden', '-g', '!.git'},
+      hidden = true,
+      -- needed to exclude some files & dirs from general search
+      -- when not included or specified in .gitignore
+      find_command = {
+        "rg",
+        "--files",
+        "--hidden",
+        "--glob=!**/.git/*",
+        "--glob=!**/.idea/*",
+        "--glob=!**/.vscode/*",
+        "--glob=!**/build/*",
+        "--glob=!**/dist/*",
+        "--glob=!**/yarn.lock",
+        "--glob=!**/package-lock.json",
+      },
     },
   },
 	-- extension
