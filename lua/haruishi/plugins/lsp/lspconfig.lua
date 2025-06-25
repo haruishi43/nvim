5b36a66b4de1060f60839067709985bc70626190
Toraudonn <toraudonn@gmail.com>
2025-06-25 15:42:24 +0900

    update

--- lua/haruishi/plugins/lsp/lspconfig.lua
+++ lua/haruishi/plugins/lsp/lspconfig.lua
@@ -70,7 +70,7 @@ lspconfig["ts_ls"].setup({
 })
 
 -- configure ruff
-require('lspconfig').ruff.setup({
+lspconfig["ruff"].setup({
   init_options = {
     settings = {
       -- Ruff language server settings go here
