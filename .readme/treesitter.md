faf126c24c915971b390be65e62ee84b70879507
Toraudonn <toraudonn@gmail.com>
2026-03-06 11:15:44 +0900

    remove avante

---
+++ .readme/treesitter.md
@@ -0,0 +1,7 @@
+
+When things fail:
+```
+```
+nvim --headless -c "TSUpdate" -c "sleep 30" -c "qa"
+```
+```
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -10,5 +10,17 @@ vim.opt.listchars:append({
 
 return {
   { import = "lazyvim.plugins.editor" },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+    },
+  },
 }
 
